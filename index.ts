1fe46131144366d736e2465fe95c9db810d9dbff
germangguerci <germangguerci@gmail.com>
2021-05-09 14:22:54 -0300

    Before hapi implementation

---
+++ index.ts
@@ -0,0 +1,66 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+/*     await prisma.user.create({
+        data: {
+          name: 'German',
+          email: 'germangguerci@gmail.com',
+          username: 'germangguerci',
+          password: 'Tobogan2020!',
+          tasks: {
+            create: { 
+                title: 'Finalizar tutorial basico',
+                description: 'Hacer encajar las piezas de prisma con una aplicación basica',
+                status: 'DOING'
+            },
+          },
+        },
+      })  
+  const allUsers = await prisma.user.findMany({
+      include: {
+          tasks: true
+      }
+  })
+
+  console.dir(allUsers, {depth: null})
+
+  const taskUpdate = await prisma.task.update({
+    where: {id: 1},
+    data: {
+      status: 'CLOSED'
+    },
+  })
+  console.log(taskUpdate); */
+
+/*   await prisma.task.create({
+    data: {
+      title: "Probar agregado de task", 
+      description: "Agregar una nueva tarea y probar funcion de agregacion",
+      status: "DOING",
+      user: {
+        connect:  {id: 1}
+      }
+    }
+  }) */
+
+  const userTasks = prisma.task.aggregate({
+    count: {
+      id: true,
+    },
+    where: {
+      userId: 1
+    }
+  })
+  console.log("Usertasks", (await userTasks).count.id);
+}
+
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
---
+++ prisma/migrations/20210509002104_init/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('OPEN', 'DOING', 'CLOSED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.username_unique" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
